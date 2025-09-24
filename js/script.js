67ba1e6fd2956e4f0e6ae12630f6ce1eded5df6d
rycohutt <ryan.cohutt@uconn.edu>
2025-09-24 13:55:09 -0400

    Working api fetching

--- js/script.js
+++ js/script.js
@@ -9,6 +9,13 @@ let gridBtn = document.querySelector("#grid-btn");
 let plantGridBtn = document.querySelector("#plant-grid-btn");
 let plantCard = document.querySelectorAll(".plant-card")
 let plantInfoScreen = document.querySelector("#plant-info")
+let addPlantBtn = document.querySelector("#add-plants-btn")
+let addPlantBtn2 = document.querySelector("#plant-screen-add-btn")
+let addPlantCard = document.querySelector("#add-plant-card")
+let addPlantImage = document.querySelector("#add-plant-image")
+let addPlantInfo = document.querySelector("#add-plant-info")
+let addPlantCloseBtn = document.querySelector("#add-plant-close-btn")
+let plantCameraBtn = document.querySelector("#add-camera")
 
 window.addEventListener('load', () => {
     setTimeout(() => {
@@ -92,20 +99,43 @@ window.addEventListener('load', () => {
         elements[i].addEventListener('click', eachPlantPage, false);
     }
 
-    // document.body.addEventListener('click', function (evt) {
-    // if (evt.target.className === 'plant-card') {
-    //     plantScreen.style.opacity = '0';
-    //     homeScreen.style.opacity = '0';
-    //     titleBar.style.opacity = '0';
-    //     setTimeout(() => {
-    //         plantScreen.style.display = 'none';
-    //         homeScreen.style.display = 'none';
-    //         titleBar.style.display = 'none';
-    //         plantInfoScreen.style.display = 'grid';
-    //         setTimeout(() => {
-    //             plantInfoScreen.style.opacity = '1';
-    //         }, 15);
-    //     }, 300);
-    // }
-    // }, false);
+    addPlantBtn.addEventListener('click', () => {
+        addPlantCard.style.display = 'grid';
+        setTimeout(() => {
+            addPlantCard.style.opacity = '1';
+            addPlantCard.style.bottom = '0';
+            addPlantCard.style.top = 'auto';
+        }, 300);
+    })
+   
+    addPlantBtn2.addEventListener('click', () => {
+        addPlantCard.style.display = 'grid';
+        setTimeout(() => {
+            addPlantCard.style.opacity = '1';
+            addPlantCard.style.bottom = '0';
+            addPlantCard.style.top = 'auto';
+        }, 300);
+    })
+
+    addPlantCloseBtn.addEventListener('click', () => {
+        addPlantCard.style.opacity = '0';
+        addPlantCard.style.bottom = 'auto';
+        addPlantCard.style.top = '100vh';
+        addPlantInfo.style.opacity = '0';
+        addPlantImage.style.display = 'grid';
+        setTimeout(() => {
+            addPlantCard.style.display = 'none';
+            addPlantImage.style.opacity = '1';
+            addPlantInfo.style.display = 'none';
+        }, 300);
+    })
+
+    plantCameraBtn.addEventListener('click', () => {
+        addPlantImage.style.opacity = '0';
+        addPlantInfo.style.display = 'grid';
+        setTimeout(() => {
+            addPlantInfo.style.opacity = '1';
+            addPlantImage.style.display = 'none';
+        }, 300);
+    })
 });
